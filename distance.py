4350801dccbb2ddde0d10eb31ecfa7497977c37f
DiskMonkey <MukulPlace@gmail.com>
2024-07-25 14:56:40 -0400

    distance

--- distance.py
+++ distance.py
@@ -1,27 +1,60 @@
 from geopy.distance import geodesic
 
-center = {
-	37.7937,
-	-122.3965,
-}
+upBound = 90
+downBound = -90
 
-location1 = {
-	37.793832099999996,
-	-122.3966844
-}
+rightBound = 180
+leftBound = -180
 
-location2 = {
-	37.79379110000001,
-	-122.3967923
-}
+# This dictionary stores distict mapped areas. Mapped area is always a circle, so we store the center and the radius.
+mappedDict = {} # id : (coords, radii)
 
-loc3 = {
-	37.7938153,
-	-122.39675349999999
-},
 
-print(geodesic(center, location1).km)
+# checks if a point is inside of already mapped area, or is outside the bounding box
+def isWithinUnmapped(inputCoords):
+	
+	for point in mappedDict:
+		coords, radius = mappedDict[point]
+		if (geodesic(inputCoords, coords).km < radius):
+			return False
+		
+	lat, long = inputCoords
+	return (lat < upBound and lat > downBound) and (long > leftBound and long < rightBound)
 
-print(geodesic(center, loc3).km)
 
-print(geodesic(center, location2).km)
+def distanceToNearestEdge(queryCoords):
+	lat, long = queryCoords 
+
+	smallestDist = min(
+		geodesic(queryCoords, (upBound, long)).km,
+		geodesic(queryCoords, (downBound, long)).km,
+		geodesic(queryCoords, (lat, leftBound)).km,
+		geodesic(queryCoords, (lat, rightBound)).km
+	)
+	
+	for point in mappedDict:
+		coords, radius = mappedDict[point]
+		distanceFromBoundary = geodesic(queryCoords, coords).km - radius
+		smallestDist = min(smallestDist, distanceFromBoundary)
+
+	return smallestDist
+
+divisor = 21 # heuristic for below function. larger number means more manual checking. smaller number means slower convergence. 
+numTrials = 35 # number of iterations (higher number takes longer but is more accurate)
+def findPoleOfInaccessibility(lat, long):
+	verticalSeperation = (upBound - downBound) / divisor
+	horizontalSeperation = (rightBound - leftBound) / divisor
+
+	maxDistance = -1
+	maxCoord = None
+
+	for horiNum in range((divisor + 1)):
+		for vertiNum in range((divisor + 1)):
+			coords = (downBound + (vertiNum * verticalSeperation), leftBound + (horiNum * horizontalSeperation))
+			if isWithinUnmapped(coords):
+				maxDistance = max(maxDistance, distanceToNearestEdge(coords))
+				maxCoord = coords
+
+	# i need to make it iterate. so... 
+	# i need to figure out how long/wide the original bounding box is in km, divide by 2 sqrt(2),
+	# go that much up/sideways from the maxCoord, and that will be the new bounding box
